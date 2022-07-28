a88b421e1a8ddd41b548a6fe386f90e6bbe6dc53
ramaiswara98 <ramaiswara098@gmail.com>
2022-07-29 04:08:17 +0800

    fix issue large error

--- index.js
+++ index.js
@@ -20,9 +20,10 @@ app.use((req,res,next) => {
 
     next();
 })
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(express.json());
 app.use(cors());
-app.use(express.bodyParser({limit: '50mb'}));
   
   // BodyParser Middleware
   app.use(
